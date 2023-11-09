4368c4e1d519e885cbf72614aa3265d88fcbe314
ngoquocbao1010 <totti.chelsea@gmail.com>
2023-11-09 22:53:47 +0700

    💡 feat: Product model

---
+++ products_sv/products/models.py
@@ -0,0 +1,13 @@
+import uuid
+
+from django.db import models
+
+
+class Products(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name}"
